f0b5e45c80dcebe66a282c48bf260e7991e55c31
Dior.Shelton <dior.shelton@gmail.com>
2021-09-26 14:59:54 -0400

    Add error handling for radio buttons. Add clear button functionality for suggested meals box

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let letsCookButton = document.querySelector('.lets-cook');
 let cookpot = document.querySelector('.cookpot');
 let suggestionHeader = document.querySelector('#suggestion');
 let dinnerSuggestion = document.querySelector('.dinner-suggestion');
+let clearButton = document.querySelector(".clear-btn");
 
 let sides = [
 "Miso Glazed Carrots",
@@ -59,29 +60,38 @@ form.addEventListener('submit', function(e) {
   for (const entry of radioValue) {
     selectedOption = entry[1];
   };
+  if (selectedOption !== '') {
+    clearButton.removeAttribute('id', "hide-button");
+  }
   e.preventDefault();
   displaySuggestedDish(selectedOption);
+});
+clearButton.addEventListener('click', function clearMealSuggestion() {
+  dinnerSuggestion.innerText = '';
+  suggestionHeader.style.display = 'none';
+  cookpot.style.display = "initial";
+  clearButton.setAttribute('id', "hide-button");
 });
 
 function randomSide() {
   let sideItem = Math.floor(Math.random()*sides.length);
   return sides[sideItem];
-}
+};
 function randomDessert() {
   let dessertItem = Math.floor(Math.random()*desserts.length);
   return desserts[dessertItem];
-}
+};
 function randomMainDish() {
   let mainDishItem = Math.floor(Math.random()*mains.length);
   return mains[mainDishItem];
-}
+};
 function randomEntireMeal() {
   let side = randomSide();
   let main = randomMainDish();
   let dessert = randomDessert();
   let entireMeal = `${main} with a side of ${side} and ${dessert} for dessert!`
   return entireMeal;
-}
+};
 function displaySuggestedDish(selectedOption) {
   cookpot.style.display = 'none';
   suggestionHeader.style.display = 'inherit';
@@ -94,5 +104,5 @@ function displaySuggestedDish(selectedOption) {
     dinnerSuggestion.innerText = randomDessert();
   } else {
     dinnerSuggestion.innerText = randomEntireMeal();
-  }
-}
+  };
+};
